1c6288c04dbe1fad9d03575dec115506838d4074
nmenag <nmena.garzon@gmail.com>
2020-10-26 14:01:08 -0500

    Add credo

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Cars.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5.0-rc.2", only: :dev, runttime: false}
     ]
   end
 
